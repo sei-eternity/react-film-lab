ac3c675ab315aee50c486f385d70ce2bf2c50a17
Marc Wright <marc.wright@gmail.com>
2019-08-03 09:27:51 +0300

    Step2 complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FilmDetails from './FilmDetails.js';
+import FilmListing from './FilmListing.js';
 
 function App() {
   return (
     <div className="App">
       <div className="film-library">
-        <div className="film-list">
-          <h1 className="section-title">FILMS</h1>
-        </div>
-
-        <div className="film-details">
-          <h1 className="section-title">DETAILS</h1>
-        </div>
+        <FilmListing />
+        <FilmDetails />
       </div>
     </div>
   );
