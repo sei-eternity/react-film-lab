ea8a88ede8540743d78a360f7cc03806d3862bdb
Marc Wright <marc.wright@gmail.com>
2019-08-03 09:42:15 +0300

    Step7 complete

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 class FilmRow extends Component {
   render() {
+    const year = new Date(this.props.film.release_date);
+
     return (
       <div>
-        <h1>{this.props.film.title}</h1>
+        <div className="film-row">
+          <img src={this.props.film.poster_path} alt="" />
+
+          <div className="film-summary">
+            <h1>{this.props.film.title}</h1>
+            <p>{year.getFullYear()}</p>
+          </div>
+        </div>
       </div>
     )
   }
