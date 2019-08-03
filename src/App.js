7b0fb9d65da226886591ffc2e1c4a55d8f2f18db
Marc Wright <marc.wright@gmail.com>
2019-08-03 09:30:15 +0300

    Step3 complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import FilmDetails from './FilmDetails.js';
 import FilmListing from './FilmListing.js';
+import TMDB from './TMDB.js';
 
 function App() {
   return (
     <div className="App">
       <div className="film-library">
-        <FilmListing />
-        <FilmDetails />
+        <FilmListing films={TMDB.films} />
+        <FilmDetails films={TMDB.films} />
       </div>
     </div>
   );
